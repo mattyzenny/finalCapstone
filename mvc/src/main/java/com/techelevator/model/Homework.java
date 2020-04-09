426e579e7479fa347bd1a18562f56a3302ab48bc
Bryan Wright <bryan.l.wright@gmail.com>
2020-04-09 09:50:48 -0400

    homework pojo

--- mvc/src/main/java/com/techelevator/model/Homework.java
+++ mvc/src/main/java/com/techelevator/model/Homework.java
@@ -1,5 +1,57 @@
 package com.techelevator.model;
 
+import java.util.Date;
+
 public class Homework {
+	
+	private int homeworkId;
+	private String homeworkName;
+	private Date dueDate;
+	private boolean complete;
+	private String question;
+	private String answer;
+	private int courseId;
+	public int getHomeworkId() {
+		return homeworkId;
+	}
+	public void setHomeworkId(int homeworkId) {
+		this.homeworkId = homeworkId;
+	}
+	public String getHomeworkName() {
+		return homeworkName;
+	}
+	public void setHomeworkName(String homeworkName) {
+		this.homeworkName = homeworkName;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
 
 }
--- mvc/src/main/java/com/techelevator/model/HomeworkDAO.java
+++ mvc/src/main/java/com/techelevator/model/HomeworkDAO.java
@@ -1,5 +1,10 @@
 package com.techelevator.model;
 
+import java.util.List;
+
 public interface HomeworkDAO {
+
+	public Homework getAllHomeworkByCourse();
+	public List<Homework> getHomeworkByStudentId();
 
 }
